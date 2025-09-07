97714808f71834823f6be157cea04a7155661d00
Alexander Danilov <Tyuiu.DanilovAS@outlook.com>
2025-09-08 04:18:23 +0500

    Task1

---
+++ Tyuiu.DanilovAS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.DanilovAS.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Alexander";
+            int age = 23;
+
+            Console.WriteLine("Здравствуйте меня зовут" + name + " мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
