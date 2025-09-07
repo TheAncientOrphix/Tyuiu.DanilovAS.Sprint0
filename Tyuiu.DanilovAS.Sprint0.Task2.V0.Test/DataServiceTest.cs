430309dfec764adc4cb87db06757658c7583f90f
Alexander Danilov <Tyuiu.DanilovAS@outlook.com>
2025-09-08 04:34:52 +0500

    Task2

---
+++ Tyuiu.DanilovAS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.DanilovAS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.DanilovAS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CkeckGetMessageValid()
+        {
+            var name = "Aлександр";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Aлександр", res);
+        }
+    }
+}
---
+++ Tyuiu.DanilovAS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.DanilovAS.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.DanilovAS.Sprint0.Task2.V0
+ 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Александр"));
+            Console.ReadKey();
+        }
+    }
+}
